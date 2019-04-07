3694b0b4a5e5c82f7b5fcd42b1f81d6a66dec908
svn1991 <svn1991@gmail.com>
2019-04-08 00:49:42 +0800

    add results component - wip

---
+++ src/components/Results.vue
@@ -0,0 +1,23 @@
+<template>
+  <transition name="fade">
+    <div class="results-wrapper" v-if="show">
+      Congrats, you have completed the test.
+    </div>
+  </transition>
+</template>
+
+<script>
+export default {
+  props: ['show']
+}
+</script>
+
+<style>
+.fade-enter-active, .fade-leave-active {
+  transition: opacity 0.25s ease-out 0.25s;
+}
+
+.fade-enter, .fade-leave-to {
+  opacity: 0;
+}
+</style>
