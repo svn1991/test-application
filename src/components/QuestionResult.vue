ddc6ef672fb5d6ff35163945d8516cad1234cf22
svn1991 <svn1991@gmail.com>
2019-04-08 22:27:42 +0800

    fix question answer checks

--- src/components/QuestionResult.vue
+++ src/components/QuestionResult.vue
@@ -3,7 +3,7 @@
 -->
 <template>
   <div class="question-result-wrapper">
-    <div class="result">
+    <div class="result" v-if="showOverall"> 
       <div class="pass" v-bind:class="showResult ? 'showResult' : 'hideResult'">You answered correctly. Good job!</div>
       <div class="fail" v-bind:class="!showResult ? 'showResult' : 'hideResult'">
           That was the wrong answer.<br />
@@ -20,56 +20,65 @@ export default {
   props: ['showEncourage', 'questionObject', 'candidateAnswer'],
   data () {
     return {
-      showResult: false
+      showResult: false,
+      showOverall: false
     }
   },
   /**
    * Desc: check candidate answer with pre-given answers and report back to parent component
   */
-  updated () {
-    let type = this.questionObject.type
-    let candidateAnswer = this.candidateAnswer
-    let correctAnswer = this.questionObject.answers
-
-    let record = {
-      question: this.questionObject.question,
-      candidateAnswer: candidateAnswer
-    }
-
-    let correct = 0
-    let pushToType = ''
-
-    switch (type) {
-      case 'multiple-radio':
-        if (candidateAnswer === correctAnswer) {
-          this.showResult = true
-          correct++
-        }
-        record.preDefinedAnswer = correctAnswer
-        pushToType = 'fixed'
-        break
-      case 'multiple-checkbox':
-        if (this.isArrayEqual(candidateAnswer, correctAnswer)) {
-          this.showResult = true
-          correct++
-        }
-        record.preDefinedAnswer = correctAnswer
-        pushToType = 'fixed'
-        break
-      case 'text':
-        pushToType = 'open-end'
-        break
-      default:
-        console.log('No result check for question type: ' + type)
-    }
-
-    this.$emit('returnResultStatus', {
-      record,
-      point: correct,
-      pushToType
-    })
+  mounted () {
+    this.checkResults()
   },
   methods: {
+    /**
+     * Desc: recheck results
+     */
+    checkResults () {
+      let type = this.questionObject.type
+      let candidateAnswer = this.candidateAnswer
+      let correctAnswer = this.questionObject.answers
+
+      let record = {
+        question: this.questionObject.question,
+        candidateAnswer: candidateAnswer
+      }
+
+      let correct = 0
+      let pushToType = ''
+
+      switch (type) {
+        case 'multiple-radio':
+          this.showOverall = true
+          if (candidateAnswer === correctAnswer) {
+            this.showResult = true
+            correct++
+          }
+          record.preDefinedAnswer = correctAnswer
+          pushToType = 'fixed'
+          break
+        case 'multiple-checkbox':
+          this.showOverall = true
+          if (this.isArrayEqual(candidateAnswer, correctAnswer)) {
+            this.showResult = true
+            correct++
+          }
+          record.preDefinedAnswer = correctAnswer
+          pushToType = 'fixed'
+          break
+        case 'text':
+          pushToType = 'open-end'
+          break
+        default:
+          console.log('No result check for question type: ' + type)
+      }
+
+      this.$emit('returnResultStatus', {
+        record,
+        point: correct,
+        pushToType
+      })
+    },
     /**
      * Desc: Check if two arrays are equal
      */
